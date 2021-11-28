697fed377fc1d3681e5b8926c6cab0707bef69e7
aorebel <aorebels@gmail.com>
2021-11-28 08:59:53 +0800

    wifor heroku initial deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
+import node from  "@sveltejs/adapter-node";
 import adapter from '@sveltejs/adapter-auto';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
+		adapter: node({ env: { port: process.env.PORT } }),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
