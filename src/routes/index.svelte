11380c5335f6ca4d837d46f79a53d12de020a389
aorebel <aorebels@gmail.com>
2021-12-04 21:39:43 +0800

    second version of frontend design

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,16 @@
 <script>
     import Hero from "$lib/components/homepage/hero.svelte";
+    import Carrrouseel from "$lib/components/homepage/caroussel.svelte";
     import Profile from "$lib/components/homepage/profile.svelte";
     import Gallery from "$lib/components/homepage/gallery.svelte";
     import Instagram from "$lib/components/homepage/instagram.svelte";
     import AutoPost from "$lib/services/instagram-post.svelte";
+import Caroussel from "$lib/components/homepage/caroussel.svelte";
 </script>
 
 <Hero />
+<Caroussel />
+
 <main id="main">
     <Profile />
     <Gallery />
